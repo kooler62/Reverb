38022d4c21c9333d3c3436172cd424ab1105ae09
volodymyr <kooler62@gmail.com>
2026-03-10 23:02:15 +0200

    edit seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,15 +8,25 @@ use Illuminate\Database\Seeder;
 class UserSeeder extends Seeder
 {
     protected const DEMO_USER_EMAIL = 'test@example.com';
+    protected const DEMO_USER_1_EMAIL = 'test1@example.com';
 
     public function run(): void
     {
         $demoUserExists = User::query()->where('email', self::DEMO_USER_EMAIL)->exists();
+        $demoUser1Exists = User::query()->where('email', self::DEMO_USER_1_EMAIL)->exists();
 
-        if (!$demoUserExists) {
+
+        if (!$demoUser1Exists) {
             User::factory()->create([
                 'name' => 'Test User',
                 'email' => self::DEMO_USER_EMAIL,
+            ]);
+        }
+
+        if (!$demoUser1Exists) {
+            User::factory()->create([
+                'name' => 'Test User 1',
+                'email' => self::DEMO_USER_1_EMAIL,
             ]);
         }
 
