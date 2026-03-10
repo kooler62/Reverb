9d8931cc30b2014b80813357a82912e8dc7a92cc
volodymyr <kooler62@gmail.com>
2026-03-10 22:14:43 +0200

    users page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController extends Controller
 {
     public function index(): View
     {
-        $users = User::paginate(10);
+        $users = User::query()->paginate();
 
         return view('users.index', compact('users'));
     }
