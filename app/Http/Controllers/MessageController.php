346154e6b192b9c8606eaebf6ae357f954240678
volodymyr <kooler62@gmail.com>
2026-03-10 22:39:32 +0200

    edit

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -9,7 +9,10 @@ class MessageController extends Controller
 {
     public function index(): View
     {
+        $authUser = auth()->user();
         $messages = Message::query()
+            ->where('receiver_id', $authUser->id)
+            ->orWhere('sender_id', $authUser->id)
             ->with(['sender', 'receiver'])
             ->latest()
             ->paginate();
