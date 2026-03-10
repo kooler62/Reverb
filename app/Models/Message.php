e85d562f47158700e10c7c766e409d182ae3735c
volodymyr <kooler62@gmail.com>
2026-03-10 21:57:15 +0200

    edit model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -5,7 +5,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property int $id
+ * @property ?int $sender_id
+ * @property ?int $receiver_id
+ * @property ?string $text
+ * @property bool $is_read
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property-read ?User $sender
+ * @property-read ?User $receiver
+ */
 class Message extends Model
 {
     protected $fillable = [
