780e5b905207b9567824ce52d8aa219f5397c7cc
volodymyr <kooler62@gmail.com>
2026-03-10 23:09:10 +0200

    message service

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Message;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+
+class MessageSent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets;
+
+    public function __construct(
+        public readonly Message $message,
+    ) {}
+
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('messages.' . $this->message->receiver_id),
+        ];
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'id' => $this->message->id,
+            'sender_id' => $this->message->sender_id,
+            'sender_name' => $this->message->sender?->name,
+            'text' => $this->message->text,
+            'created_at' => $this->message->created_at->toISOString(),
+        ];
+    }
+}
